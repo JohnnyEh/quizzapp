47ac2a7da4893c9a383e61100c2bd5eef60bfb62
JohnnyEh <jangell316@gmail.com>
2016-07-25 07:32:02 -0400

    changed branch and added js

---
+++ app.js
@@ -0,0 +1,36 @@
+var question = [{
+	statement: "What did Al eat every day for breakfast?",
+    answers: ["Scrambled Eggs", "Sauerkraut", "Pickled Cucumbers", "Spam, spam, spam, spam, baked beans and spam"],
+    quesNum: 0,
+    corrAns: 2
+},
+{
+	statement: "How did Al survive the plane crash?",
+    answers: ["His superpowers kicked in", "The pilot pulled him from the wreckage", "He had his tray table up and his seat back in the full upright position", "Using the seat as a floatation device"],
+    quesNum: 1,
+    corrAns: 3
+},
+{
+	statement: "Al buys a dozen box of what from the donut shop?",
+	answers: ["Warm, delicious donuts", "Starving, crazed weasels", "Mangy, rabid cats", "Slimy, slithering snakes"],
+	quesNum: 2,
+	corrAns: 2
+},
+{
+	statement: "Al luckily runs into the girl of his dreams.  What is her name?",
+	answers: ["Daisy", "Peach", "Samus", "Zelda"]
+	quesNum: 3,
+	corrAns: 4
+},
+{
+	statement: "What is Al's life-long dream?",
+	answers: ["Lazer-tag arena manager", "Basketball player", "Part-time job at the Sizzler", "Substitute teacher"],
+	quesNum: 4,
+	corrAns: 3
+}]
+
+var results = {
+	poor: "Man, I knew you were gonna suck at this quiz. At least listen to the song before trying again.",
+	average: "Yeah, and if a million monkeys typed on a million typewriters for a million years they'd eventually type out the works of Spielberg.  Or something like that.",
+	excellent: "Wow, man! That was totally amazing!  Wakka Wakka doo doo Yeah!!"
+}
